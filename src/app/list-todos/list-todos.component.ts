f76fa722dc1171b273be0bd6fa9ccef740ab9c60
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-10-03 17:46:44 +0530

    Step 22 - Best Practice - Create a Todo

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+//blueprint class of object
+export class Todo{
+  constructor(
+        public id:number,
+        public description:string,
+        public done:boolean,
+        public targetDate:Date
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -9,15 +21,12 @@ export class ListTodosComponent implements OnInit {
 
   //hard coded todo data
   todos = [
-    {id: "1",description: "Angular Course"},
-    {id: "2",description: "Springboot Course"},
-    {id: "3",description: "Python Course"}
+    new Todo(1,'Angular',false,new Date()),
+    new Todo(2,'React',true,new Date()),
+    new Todo(3,'java',true,new Date()),
+    new Todo(4,'Typecript',false,new Date())
   ];
 
-  todo = {
-    id: "1",
-    description: "Angular Course"
-  }
 
   constructor() { }
 
