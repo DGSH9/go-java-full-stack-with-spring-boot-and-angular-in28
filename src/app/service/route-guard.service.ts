2eb693ec8803be32ca557d64e032cbb03ee7b85e
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-10-04 12:36:13 +0530

    35 - Securing Components using Route Guards - Part 1

---
+++ src/app/service/route-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { HardCodedAuthenticationService } from './hard-coded-authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouteGuardService implements CanActivate {
+
+  constructor(private hardCodedAuthenticationService: HardCodedAuthenticationService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.hardCodedAuthenticationService.isUserLoggedIn()) {
+      return true;
+    }
+    return false;
+  }
+
+}
