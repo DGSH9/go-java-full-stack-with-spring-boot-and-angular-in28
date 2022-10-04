a129523526cff1ca45bd9b5d61634bf82dcea5d6
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-10-04 11:23:13 +0530

    32 - Using Session Storage to Store User Authentication Token

--- src/app/service/hard-coded-authentication.service.ts
+++ src/app/service/hard-coded-authentication.service.ts
@@ -9,12 +9,22 @@ export class HardCodedAuthenticationService {
 
   //creating method for authentication
   authenticate(username: string, password: string) {
+    console.log("before" + this.isUserLoggedIn());
+
     if (username === 'dgsh' && password === 'dgsh') {
+      console.log("after" + this.isUserLoggedIn());
+
+      sessionStorage.setItem('authenticateUser', username);
       return true;
     }
     else {
       return false;
     }
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticateUser');
+    return !(user === null);
   }
 
 
