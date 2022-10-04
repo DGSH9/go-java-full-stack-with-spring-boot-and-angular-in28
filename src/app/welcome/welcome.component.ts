a6d5b5a81f9ef0c092b73706d2e476481454629b
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-10-04 13:56:24 +0530

    50 - Connecting Angular Frontend with Restful API - 1 - Creating Data Servi

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -13,12 +14,19 @@ export class WelcomeComponent implements OnInit {
   name = '';
   //deafult constructor
   //AcivatedRoute
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private welcomeDataService: WelcomeDataService) { }
 
   ngOnInit(): void {
     console.log(this.message);
     // console.log(this.route.snapshot.params['name']);
     this.name = this.route.snapshot.params['name'];
+  }
+
+  //getWelcomeMessage
+  getWelcomeMessage() {
+    // console.log("get welcome message");
+    this.welcomeDataService.executeHelloWorldService();
   }
 
 }
